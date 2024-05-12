e88e3c7651d311feca49bcc7fa3ecd11e656b694
Max Slobodyanuk <119804658+MaxSlobodynuk@users.noreply.github.com>
2024-05-12 22:52:18 +0300

    add localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import ContactForm from "../ContactForm/ContactForm";
 import ContactList from "../ContactList/ContactList";
 import SearchBox from "../SearchBox/SearchBox";
 import css from "./App.module.css";
 
-// const contacts = [
-//   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-//   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-//   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-//   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-// ];
+const getContacts = () => {
+  const saveContacts = localStorage.getItem("contacts");
+  if (saveContacts !== null) {
+    return JSON.parse(saveContacts);
+  }
+  return []
+};
+
 
 const App = () => {
-  const [contactList, setContactList] = useState([]);
+  const [contactList, setContactList] = useState(getContacts);
   const [inputValue, setInputValue] = useState("");
-
-  console.log(contactList);
 
   const handleInput = ({ target }) => {
     setInputValue(target.value);
@@ -34,8 +34,13 @@ const App = () => {
 
   const handleDelate = (id) => {
     const contacts = contactList.filter((contact) => contact.id !== id);
-    console.log(contacts);
+    setContactList(contacts);
   };
+
+    useEffect(() => {
+      localStorage.setItem("contacts", JSON.stringify(contactList));
+    }, [contactList]);
+
 
   const filteredContacts = contactList.filter((contact) =>
     contact.name.toLowerCase().includes(inputValue.toLowerCase())
